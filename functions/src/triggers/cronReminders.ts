b9431a2e329a37c839ee5023537579d9ff5e4008
Hugo Gresse <hugo.gresse@gmail.com>
2020-01-06 11:57:20 +0100

    update reminder cron to run every hour

--- functions/src/triggers/cronReminders.ts
+++ functions/src/triggers/cronReminders.ts
@@ -2,8 +2,7 @@ import * as functions from "firebase-functions";
 import { checkAndProcessReminders } from "../reminder/checkAndProcessReminders";
 
 export const cronReminders = functions.pubsub
-  // hour : 0 * * * *
-  .schedule("*/5 * * * *")
+  .schedule("0 * * * *")
   .onRun(async context => {
     await checkAndProcessReminders();
     return null;
