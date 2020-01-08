ec30294daabe270e151cd35c5d2fa76f683a857c
Hugo Gresse <hugo.gresse@gmail.com>
2020-01-08 16:38:20 +0100

    Fix api routing in production

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -1,10 +1,10 @@
-import * as functions from "firebase-functions";
-import * as express from "express";
-import routerV1 from "./v1";
+import * as functions from 'firebase-functions'
+import * as express from 'express'
+import routerV1 from './v1'
 
-const app = express();
+const app = express()
 
 // API router v1 (/api/ is added on main index.ts export)
-app.use("/v1", routerV1);
+app.use('/api/v1', routerV1)
 
-export const api = functions.runWith({}).https.onRequest(app);
+export const api = functions.runWith({}).https.onRequest(app)
