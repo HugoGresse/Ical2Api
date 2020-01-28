7eb09b2504dd6a373f79439d46ca955b2dcc4f1e
Hugo Gresse <hugo.gresse@gmail.com>
2020-01-28 15:32:53 +0100

    Enable CORS on API

--- functions/src/api/v1/index.ts
+++ functions/src/api/v1/index.ts
@@ -1,9 +1,12 @@
 import * as express from 'express'
+import * as cors from 'cors'
 import getEvents from './getEvents'
 import getIcals from './getIcals'
 
 // router
 const router = express.Router()
+
+router.use(cors({ origin: true }))
 
 // Events APIs
 router.get('/events/', getEvents)
