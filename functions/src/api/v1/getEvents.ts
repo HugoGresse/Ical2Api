5b034a1d810a5169d7dc7c73ab555b38e5fcea5a
Hugo Gresse <hugo.gresse@gmail.com>
2020-01-29 18:48:36 +0100

    Fix organizations not being managed in the api on events endpoint

--- functions/src/api/v1/getEvents.ts
+++ functions/src/api/v1/getEvents.ts
@@ -4,12 +4,16 @@ import * as admin from 'firebase-admin'
 import Query = admin.firestore.Query
 
 export default async (req: Request, res: Response) => {
-    const { icals, status } = req.query
+    const { icals, status, organizations } = req.query
 
     let query: Query = db.collection('events')
 
     if (icals) {
         query = query.where('icalId', 'in', icals.split(','))
+    }
+
+    if (organizations) {
+        query = query.where('organizationId', 'in', organizations.split(','))
     }
 
     if (status) {
