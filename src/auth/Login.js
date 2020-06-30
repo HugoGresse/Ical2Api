d185f5d379fff8fdd771e8c2ffe68c8cc7e67e50
Hugo Gresse <hugo.gresse@gmail.com>
2020-06-30 21:48:02 +0200

    Use sign in link + update env

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyledFirebaseAuth } from 'react-firebaseui'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 import { useStateValue } from '../state/state'
 import { Link } from 'react-router-dom'
 
@@ -9,7 +9,30 @@ const uiConfig = {
     signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        {
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            // Use email link authentication and do not require password.
+            // Note this setting affects new users only.
+            // For pre-existing users, they will still be prompted to provide their
+            // passwords on sign-in.
+            signInMethod:
+                firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+            // Allow the user the ability to complete sign-in cross device, including
+            // the mobile apps specified in the ActionCodeSettings object below.
+            forceSameDevice: false,
+            // Used to define the optional firebase.auth.ActionCodeSettings if
+            // additional state needs to be passed along request and whether to open
+            // the link in a mobile app if it is installed.
+            emailLinkSignIn: function() {
+                return {
+                    url: process.env.REACT_APP_DOMAIN,
+                    // Custom FDL domain.
+                    dynamicLinkDomain: process.env.REACT_APP_LOGIN_EMAIL_FDL,
+                    // Always true for email link sign-in.
+                    handleCodeInApp: true,
+                }
+            },
+        },
     ],
     callbacks: {
         signInSuccessWithAuthResult: () => false,
